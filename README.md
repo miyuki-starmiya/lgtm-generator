5553eb41f92969819a798fe613e968324ba8bd89
StarmiyaMiyuki <sousaku_0912@yahoo.co.jp>
2025-01-18 11:31:05 +0900

    add README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lgtm-generator
+You set input image then output LGTM image resized specific width.
+
+## How to use
+in local
+```sh
+# Specify relative path
+go run . --input input-image
+```
--- main.go
+++ main.go
@@ -12,30 +12,36 @@ import (
 
 func main() {
 	// Parse flags
-	inputPath := flag.String("input", "", "Input image path (gif, png, jpg, etc.)")
-	outputPath := flag.String("output", "output", "Output path (gif, png, jpg, etc.)")
+	basePath := flag.String("input", "", "Input image path (gif, png, jpg, etc.)")
 	lgtmPath := flag.String("lgtm", "assets/lgtm.png", "Path to LGTM overlay image")
 	width := flag.Int("width", 500, "Target width for resizing")
 	flag.Parse()
 
-	if *inputPath == "" || *outputPath == "" {
-		log.Fatal("You must specify --input and --output")
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get working directory: %v", err)
+	}
+	inputPath := fmt.Sprintf("%s/%s", wd, *basePath)
+	outputPath := fmt.Sprintf("output/%s", filepath.Base(*basePath))
+
+	if inputPath == "" {
+		log.Fatal("You must specify --input")
 	}
 	if _, err := os.Stat(*lgtmPath); os.IsNotExist(err) {
 		log.Fatalf("LGTM image does not exist: %s", *lgtmPath)
 	}
 
 	// Detect GIF by file extension
-	ext := strings.ToLower(filepath.Ext(*inputPath))
+	ext := strings.ToLower(filepath.Ext(inputPath))
 	switch ext {
 	case ".gif":
-		err := ProcessAnimatedGIF(*inputPath, *outputPath, *lgtmPath, *width)
+		err := ProcessAnimatedGIF(inputPath, outputPath, *lgtmPath, *width)
 		if err != nil {
 			log.Fatalf("GIF processing failed: %v", err)
 		}
 		fmt.Println("Animated GIF generated successfully.")
 	default:
-		err := ProcessStaticImage(*inputPath, *outputPath+ext, *lgtmPath, *width)
+		err := ProcessStaticImage(inputPath, outputPath, *lgtmPath, *width)
 		if err != nil {
 			log.Fatalf("Static processing failed: %v", err)
 		}
