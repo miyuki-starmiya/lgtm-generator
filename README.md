f331f58796b3eab111c0643e423b8c7ff858106f
StarmiyaMiyuki <sousaku_0912@yahoo.co.jp>
2025-01-30 16:24:57 +0900

    update to handle 500 and 320 width

--- README.md
+++ README.md
@@ -13,7 +13,13 @@ in local.
 go run . --input input-image
 ```
 
+### Args
+```sh
+--input input path in input/
+--width target width
+```
+
 ### Default
-- output path: ./output/lgtm_${input-image}
-- lgtm path: ./assets/lgtm.png
-- resized width: 500px
+- output path: ./output/${width}_lgtm_${input-image}
+- lgtm path: ./assets/500_lgtm.png
+- target width: 500px
--- main.go
+++ main.go
@@ -11,37 +11,49 @@ import (
 )
 
 func main() {
+	var (
+		lgtmPath string
+		width    int
+	)
+
 	// Parse flags
 	basePath := flag.String("input", "", "Input image path (gif, png, jpg, etc.)")
-	lgtmPath := flag.String("lgtm", "assets/lgtm.png", "Path to LGTM overlay image")
-	width := flag.Int("width", 500, "Target width for resizing")
+	// lgtmFlag := flag.String("lgtm", "assets/lgtm.png", "Path to LGTM overlay image")
+	targetWidthFlag := flag.Int("width", 500, "Target width for resizing")
 	flag.Parse()
 
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Failed to get working directory: %v", err)
 	}
-	inputPath := fmt.Sprintf("%s/%s", wd, *basePath)
-	outputPath := fmt.Sprintf("output/lgtm_%s", filepath.Base(*basePath))
+	inputPath := fmt.Sprintf("%s/input/%s", wd, *basePath)
+	outputPath := fmt.Sprintf("output/%d_lgtm_%s", *targetWidthFlag, filepath.Base(*basePath))
 
 	if inputPath == "" {
 		log.Fatal("You must specify --input")
 	}
-	if _, err := os.Stat(*lgtmPath); os.IsNotExist(err) {
-		log.Fatalf("LGTM image does not exist: %s", *lgtmPath)
+	if *targetWidthFlag == 500 {
+		lgtmPath = "assets/500_lgtm.png"
+		width = 500
+	} else {
+		lgtmPath = fmt.Sprintf("assets/%d_lgtm.png", *targetWidthFlag)
+		width = *targetWidthFlag
+	}
+	if _, err := os.Stat(lgtmPath); os.IsNotExist(err) {
+		log.Fatalf("LGTM image does not exist: %s", lgtmPath)
 	}
 
 	// Detect GIF by file extension
 	ext := strings.ToLower(filepath.Ext(inputPath))
 	switch ext {
 	case ".gif":
-		err := ProcessAnimatedGIF(inputPath, outputPath, *lgtmPath, *width)
+		err := ProcessAnimatedGIF(inputPath, outputPath, lgtmPath, width)
 		if err != nil {
 			log.Fatalf("GIF processing failed: %v", err)
 		}
 		fmt.Println("Animated GIF generated successfully.")
 	default:
-		err := ProcessStaticImage(inputPath, outputPath, *lgtmPath, *width)
+		err := ProcessStaticImage(inputPath, outputPath, lgtmPath, width)
 		if err != nil {
 			log.Fatalf("Static processing failed: %v", err)
 		}
